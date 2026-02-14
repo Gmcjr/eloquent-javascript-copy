28719ffee702dabe975d614a0eb3edce70cbbb21
Gmcjr <gmcostajr@gmail.com>
2026-02-14 10:32:48 -0600

    describe work

--- chapter-04/exercises.js
+++ chapter-04/exercises.js
@@ -123,8 +123,23 @@ function nth(list, n) {
 // deepEqual ///////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////
 
-function deepEqual() {
+function deepEqual(val1, val2) {
+  if (val1 === val2) return true;
+  
+  if (val1 == null || val2 == null) return false;
+  if (typeof val1 != 'object' || typeof val2 != 'object') return false;
 
+  let keys1 = Object.keys(val1);
+  let keys2 = Object.keys(val2);
+
+  if (keys1.length != keys2.length) return false;
+
+  for (let key of keys1) {
+    if (!keys2.includes(key) || !deepEqual(val1[key], val2[key])) {
+      return false;
+    }
+  }
+  return true;
 }
 
 ////////////////////////////////////////////////////////////////////////////////
